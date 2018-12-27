ba7d869f2ee1e13ff007b882c7be266711ff63be
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-12-27 15:37:47 -0800

    sf

---
+++ src/components/NavigationMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NavigationMenu extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Navbar</h1>
+      </div>
+    );
+  }
+}
+
+export default NavigationMenu;
